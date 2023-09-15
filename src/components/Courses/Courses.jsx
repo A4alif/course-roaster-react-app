691a29257d1e15c583c7a83e4742ea72d5f29ce7
alifhasanshah <alifhasanshah@gmail.com>
2023-09-16 03:07:03 +0600

    data fetch from jason file and display data dynamically

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,8 +1,15 @@
-import python from "../../assets/python.png";
 import { BsCurrencyDollar, BsBook } from "react-icons/bs";
 import Cart from "../Cart/Cart";
+import { useEffect, useState } from "react";
 
 const Courses = () => {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
+
   return (
     <>
       <main>
@@ -14,7 +21,40 @@ const Courses = () => {
               {/* grid container */}
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {/* single card */}
-                <div className="card card-compact  bg-base-100 shadow-xl">
+                {courses.map((course) => (
+                  <div
+                    key={course.id}
+                    className="card card-compact  bg-base-100 shadow-xl"
+                  >
+                    <figure className="pt-4">
+                      <img src={course.img} alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title text-lg font-semibold">
+                        {course.title}
+                      </h2>
+                      <p className="text-sm font-normal leading-6">
+                        {course.desc}
+                      </p>
+                      <div className="flex justify-between text-base text-gray-600 py-4">
+                        <div className="flex items-center space-x-3">
+                          <BsCurrencyDollar />
+                          <span>Price: {course.price}</span>
+                        </div>
+                        <div className="flex items-center space-x-3">
+                          <BsBook />
+                          <p>Credit: {course.credit}hr</p>
+                        </div>
+                      </div>
+                      <div className="card-actions  ">
+                        <button className="btn btn-info w-full text-white">
+                          Select
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+                {/* <div className="card card-compact  bg-base-100 shadow-xl">
                   <figure className="pt-4">
                     <img src={python} alt="Shoes" />
                   </figure>
@@ -41,8 +81,7 @@ const Courses = () => {
                       <button className="btn btn-primary w-full">Select</button>
                     </div>
                   </div>
-                </div>
-                
+                </div> */}
               </div>
             </div>
             {/* right side */}
